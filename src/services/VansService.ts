b72b4ec38a7dcbda527d079bdcd7d6ac8b791b8e
Glaucielle Sá <glauciellesa@gmail.com>
2023-03-09 12:57:02 +0100

    Add service as communication layer with external API

---
+++ src/services/VansService.ts
@@ -0,0 +1,19 @@
+import { Van } from "../models/Van";
+/* Camada de serviço que acessara a API(Back-end) */
+
+const URL_BASE = "http://localhost:4000/vans";
+
+export const getVans = async (): Promise<Van[]> => {
+  const response = await fetch(URL_BASE);
+  return response.json();
+};
+
+export const getVansByType = async (type: string): Promise<Van[]> => {
+  const response = await fetch(`${URL_BASE}?type=${type}`);
+  return response.json();
+};
+
+export const getVan = async (id: number): Promise<Van> => {
+  const response = await fetch(`${URL_BASE}/${id}`);
+  return response.json();
+};
