c4da875bd2b3d2a1e75413c4e3aa96bb9568aac3
Glaucielle Sá <glauciellesa@gmail.com>
2023-03-13 12:24:25 +0100

    Add style to error page

--- src/pages/PageNotFound.tsx
+++ src/pages/PageNotFound.tsx
@@ -1,13 +1,16 @@
 import { Link } from "react-router-dom";
 import styled from "styled-components";
+import Button from "../components/Button/Button";
 
 const PageNotFound = () => {
   return (
     <StyledPageNotFound>
-      <h2>Page not found!</h2>
-      <p>
-        Go to <Link to="/">Homepage</Link>.
-      </p>
+      <p>Sorry, the page you were looking for was not found.</p>
+      <div className="error_button">
+        <Button>
+          <Link to="/"> Return to home</Link>.
+        </Button>
+      </div>
     </StyledPageNotFound>
   );
 };
@@ -15,16 +18,27 @@ const PageNotFound = () => {
 export default PageNotFound;
 
 const StyledPageNotFound = styled.div`
-  padding: 1.3rem;
-  h2 {
-    color: #e17654;
-    font-weight: bold;
-    size: 1.5rem;
-    padding: 2rem 1rem;
-  }
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  padding: 1rem;
+  height: fit-content;
   p {
     color: #161616;
-    font-weight: 500;
-    text-align: justify;
+    font-weight: 700;
+    font-size: 1.8rem;
+    padding: 2rem 1rem;
+  }
+  .error_button {
+    background-color: #161616;
+    border-radius: 0.3rem;
+    align-content: center;
+    padding: 0.4rem;
+    display: flex;
+    justify-content: center;
+    a {
+      text-decoration: none;
+      color: #fff;
+    }
   }
 `;
