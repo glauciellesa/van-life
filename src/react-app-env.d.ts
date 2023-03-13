e04ffa695125023887c5c36a87cc13f4b14050b4
Glaucielle Sá <glauciellesa@gmail.com>
2023-03-13 18:12:08 +0100

    File to resolve type declaration img

---
+++ src/react-app-env.d.ts
@@ -0,0 +1,5 @@
+/* Resolve Cannot find module 'file-name.png' or its corresponding type declarations - typescript react */
+declare module "*.png";
+declare module "*.svg";
+declare module "*.jpeg";
+declare module "*.jpg";
