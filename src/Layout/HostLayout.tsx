032c6118e37e25f11b3c7c61b6d7a56ae6b6b695
Glaucielle Sá <glauciellesa@gmail.com>
2023-03-16 15:53:34 +0100

    Set up of host layout

---
+++ src/Layout/HostLayout.tsx
@@ -0,0 +1,33 @@
+import { NavLink, Outlet } from "react-router-dom";
+import styled from "styled-components";
+
+const HostLayout = () => {
+  return (
+    <StyledNav>
+      <nav className="menu_host">
+        <NavLink to="dashboard">Dashboard</NavLink>
+        <NavLink to="income">Income</NavLink>
+        <NavLink to="vans">Vans</NavLink>
+        <NavLink to="reviews">Reviews</NavLink>
+      </nav>
+      <Outlet />
+    </StyledNav>
+  );
+};
+
+export default HostLayout;
+
+const StyledNav = styled.nav`
+  padding-top: 2rem;
+  .menu_host a {
+    padding: 1rem;
+    padding-left: 1rem;
+    color: #4d4d4d;
+    font-weight: 400;
+  }
+  .menu_host a.active {
+    color: #000;
+    text-underline-offset: 0.2rem;
+    text-decoration: underline;
+  }
+`;
---
+++ src/pages/Host/Vans/VansList.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styled from "styled-components";
+
+const VansList = () => {
+  return <StyledList>teste</StyledList>;
+};
+
+export default VansList;
+
+const StyledList = styled.div`
+  padding: 1rem;
+  background-color: #fff;
+`;
