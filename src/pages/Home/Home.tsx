cf3b4c1037befe2758d6519a8e1615a7dbc0ea86
Glaucielle Sá <glauciellesa@gmail.com>
2023-03-16 17:20:28 +0100

    Css media query to Home

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -6,19 +6,17 @@ import homeImg from "../../img/homeImg.png";
 const Home = () => {
   return (
     <StyledHome>
-      <div className="home_content">
-        <p className="home_moto">
-          You got the travel plans, we got the travel vans.
-        </p>
-        <p className="home_description">
-          Add adventure to your life by joining the #vanlife movement. Rent the
-          perfect van to make your perfect road trip.
-        </p>
-        <div className="home_button">
-          <NavLink to="vans">
-            <Button> Find your van </Button>
-          </NavLink>
-        </div>
+      <p className="home_moto">
+        You got the travel plans, we got the travel vans.
+      </p>
+      <p className="home_description">
+        Add adventure to your life by joining the #vanlife movement. Rent the
+        perfect van to make your perfect road trip.
+      </p>
+      <div className="home_button">
+        <NavLink to="vans">
+          <Button> Find your van </Button>
+        </NavLink>
       </div>
     </StyledHome>
   );
@@ -27,13 +25,9 @@ const Home = () => {
 export default Home;
 
 const StyledHome = styled.div`
-  width: 100%;
+  width: 100vw;
   height: 100%;
   color: #fff;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
   padding: 2rem;
   position: relative;
   background-image: url(${homeImg});
@@ -42,8 +36,34 @@ const StyledHome = styled.div`
   background-position: center;
   background-size: cover;
   background-repeat: no-repeat;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  &:after {
+    content: "";
+    position: absolute;
+    z-index: -1;
+    inset: 0;
+    background: #000;
+    opacity: 0.4;
+  }
+
+  .home_moto {
+    font-size: 2.5rem;
+    font-weight: bold;
+    padding-bottom: 2rem;
+  }
+
+  .home_description {
+    font-size: 1.2rem;
+    font-weight: normal;
+    padding-bottom: 2rem;
+  }
 
   .home_button {
+    width: 100%;
     font-weight: bold;
     display: flex;
     justify-content: center;
@@ -55,27 +75,17 @@ const StyledHome = styled.div`
       color: #fff;
     }
   }
-
-  &:after {
-    content: "";
-    position: absolute;
-    z-index: -1;
-    inset: 0;
-    background: #000;
-    opacity: 0.4;
-  }
-  @media (max-width: 500px) {
-    height: 100%;
-  }
-  .home_moto {
-    font-size: 2.5rem;
-    font-weight: bold;
-    padding-bottom: 2rem;
-  }
-
-  .home_description {
-    font-size: 1.2rem;
-    font-weight: normal;
-    padding-bottom: 2rem;
+  @media (min-width: 600px) {
+    padding: 1rem;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    .home_moto,
+    .home_description {
+      width: 50rem;
+    }
+    .home_button {
+      width: 30rem;
+    }
   }
 `;
