112443c912a8f4fcdc6b2b542e3e652cf0938868
Glaucielle Sá <glauciellesa@gmail.com>
2023-03-22 14:26:33 +0100

    Set up chart component

---
+++ src/components/UI/Chart/Chart.tsx
@@ -0,0 +1,9 @@
+import { Review } from "@/models/Review";
+
+const Chart = ({ rating }: { rating: Review[] }) => {
+  const avarageRating = 5;
+  console.log(rating);
+  return <div></div>;
+};
+
+export default Chart;
