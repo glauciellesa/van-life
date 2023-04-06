5399f1e1a9640fbf2ed767186d08e4a77e424336
Glaucielle Sá <glauciellesa@gmail.com>
2023-04-06 16:25:48 +0200

    Add ative class to navLink

--- src/Layout/HostLayout.tsx
+++ src/Layout/HostLayout.tsx
@@ -4,12 +4,10 @@ import styled from "styled-components";
 const HostLayout = () => {
   return (
     <StyledNav>
-      <nav className="menu_host">
-        <NavLink to="dashboard">Dashboard</NavLink>
-        <NavLink to="income">Income</NavLink>
-        <NavLink to="vans">Vans</NavLink>
-        <NavLink to="reviews">Reviews</NavLink>
-      </nav>
+      <NavLink to="dashboard">Dashboard</NavLink>
+      <NavLink to="income">Income</NavLink>
+      <NavLink to="vans">Vans</NavLink>
+      <NavLink to="reviews">Reviews</NavLink>
       <Outlet />
     </StyledNav>
   );
@@ -20,15 +18,18 @@ export default HostLayout;
 const StyledNav = styled.nav`
   padding-top: 2rem;
 
-  .menu_host a {
+  a {
     padding: 1rem;
     color: #4d4d4d;
     font-weight: 400;
   }
-  .menu_host a.active {
+
+  .active,
+  a:hover {
     color: #000;
     text-underline-offset: 0.2rem;
     text-decoration: underline;
+    font-weight: bold;
   }
 
   @media (min-width: 600px) {
